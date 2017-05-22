24aee8cf7ea1f5e89291326ebde50411b0edc9c0
Cleverson Lezainski <cleversonlezainski@gmail.com>
2017-05-22 11:33:45 -0300

    Banco de Dados

---
+++ Aula2205_EF_DF/Aula2205_EF_DF/Views/ListaProdutos.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Aula2205_EF_DF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2205_EF_DF.Views
+{
+    public partial class ListaProdutos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            strConnLojaEntities context = new strConnLojaEntities();
+
+            gdwListaProdutos.DataSource = context.Produtoes.ToList();
+            gdwListaProdutos.DataBind();
+        }
+    }
+}
